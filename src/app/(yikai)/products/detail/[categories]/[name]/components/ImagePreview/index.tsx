1fd4b7404aeabb623dae5c15efb8cd8c4777107d
Xusji <571839175@qq.com>
2025-04-30 02:50:46 +0800

    [products] detail ImagePreview

--- src/app/(yikai)/products/detail/[categories]/[name]/components/ImagePreview/index.tsx
+++ src/app/(yikai)/products/detail/[categories]/[name]/components/ImagePreview/index.tsx
@@ -1,13 +1,13 @@
-// import Image from 'next/image';
-// import ImageZoom from 'react-medium-image-zoom';
-// import 'react-medium-image-zoom/dist/styles.css'; // 引入样式文件
+import Image from 'next/image';
+import ImageZoom from 'react-medium-image-zoom';
+import 'react-medium-image-zoom/dist/styles.css'; // 引入样式文件
 
 function ImagePreview({ image }: { image: string }) {
   return (
     <div>ImagePreview{image}</div>
-    // <ImageZoom>
-    //   <Image src={image} width={800} height={600} alt='' className='object-cover' />
-    // </ImageZoom>
+    <ImageZoom>
+      <Image src={image} width={800} height={600} alt='' className='object-cover' />
+    </ImageZoom>
   );
 }
 
