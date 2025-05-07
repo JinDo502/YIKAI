e9e42175b6e7aa7123dffce4737aede59fbca42b
Xusji <571839175@qq.com>
2025-05-07 16:37:22 +0800

    chore: menu

--- public/ProductsConstant/index.tsx
+++ public/ProductsConstant/index.tsx
@@ -3,13 +3,14 @@ import kpHydraulicGearPump from './KP Hydraulic Gear Pump';
 import pto from './PTO';
 import tentsukiHoist from './Tentsuki Hoist';
 import SpecialHydraulicProducts from './Special Hydraulic Products';
-
+import CustomMade from './Custom-made';
 const productsMap = {
   'Parker Hydraulic Gear Pump': parkerHydraulicGearPump,
   'KP Hydraulic Gear Pump': kpHydraulicGearPump,
   PTO: pto,
   'Tentsuki Hoist': tentsukiHoist,
   'Special Hydraulic Products': SpecialHydraulicProducts,
+  'Custom-made': CustomMade,
 };
 
 export default productsMap;
